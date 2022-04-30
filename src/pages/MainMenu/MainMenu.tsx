d337dc3348d1b701c06571dfdf74b090e85b2934
Massimo Mx Cacciapaglia <massimocacciapaglia.13@gmail.com>
2022-04-30 20:14:17 +0100

    Added Years of experience

--- src/pages/MainMenu/MainMenu.tsx
+++ src/pages/MainMenu/MainMenu.tsx
@@ -4,6 +4,8 @@ import { Separator } from '../../components/Separator';
 import { Text } from '../../components/Text';
 import './MainMenu.css';
 
+const YoE = `0${new Date().getFullYear() - 2016}`;
+
 const MainMenu = () => {
   return (
     <div className="td-MainMenu">
@@ -44,6 +46,17 @@ const MainMenu = () => {
           <Text className='font-1-2'>Current Position</Text>
           <Text className='font-1 gray'>Vortex Node & Front-End Developer</Text>
         </div>
+        <Separator />
+        <div className='years-box td-border'>
+          <Text className='num font-3 '>
+            {YoE}
+          </Text>
+          <div>
+            <Text className='yoe font-1-2'>Years of Experience</Text>
+            <Text className='yoe font-1 gray'>(Since 2016)</Text>
+          </div>
+        </div>
+        <Separator />
       </div>
     </div>
   );
