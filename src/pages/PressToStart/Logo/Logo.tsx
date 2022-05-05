3bb8118b29c7297357aba130148d3e4d82933906
Massimo Mx Cacciapaglia <massimocacciapaglia.13@gmail.com>
2022-05-05 10:21:25 +0100

    clip path fun!

---
+++ src/pages/PressToStart/Logo/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Logo.style.css';
+
+export const Logo = () => {
+  return (
+    <div className={`pts-logo`}> 
+      <span className='division-ui' data-text="Division">
+        
+      </span>
+    </div>
+  );
+}
--- src/pages/PressToStart/PressToStart.tsx
+++ src/pages/PressToStart/PressToStart.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { ButtonKeybind } from '../../components/ButtonKeybind';
-import { Separator } from '../../components/Separator';
+import { Logo } from './Logo';
 import './PressToStart.style.css';
 
 export const PressToStart = () => {
   return (
     <div className={`td-pts`}>
+      <Logo />
       <div className='td-pts-buttons'>
-        <ButtonKeybind keybind='space' label='Press To Start' />
+        <ButtonKeybind keybind='space' label='Press to Start' />
         <ButtonKeybind keybind='Esc' label='Quit' />
       </div>
     </div>
